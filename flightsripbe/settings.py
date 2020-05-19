f3faf0dd677b16393f0d91918ad21e9298e87f03
Francesco Signoretti <signorettif@gmail.com>
2020-05-19 08:08:16 +0200

    Fixed some useless stuff

--- flightsripbe/settings.py
+++ flightsripbe/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url       # Place this line preferably at the top
 from decouple import config 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -25,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(config('DEBUG', True) == 'False')
+DEBUG = bool(config('DEBUG') == 'True')
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', '').split()
 
